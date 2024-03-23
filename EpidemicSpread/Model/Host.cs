fdb37cd00359683979faefa245850d5738da26d8
ersba <ersan.bara@gmail.com>
2024-03-23 17:09:31 +0100

    rollback

---
+++ EpidemicSpread/Model/Host.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using Mars.Common;
+using Mars.Common.Core.Random;
+using Mars.Interfaces.Agents;
+using Mars.Interfaces.Annotations;
+using NumSharp;
+using Mars.Interfaces.Environments;
+using Mars.Interfaces.Layers;
+using Mars.Numerics;
+using Tensorflow;
+using Tensorflow.Keras.Utils;
+using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+using Tensorflow.NumPy;
+using Shape = Tensorflow.Shape;
+
+namespace EpidemicSpread.Model 
+{
+    public class Host : IAgent<InfectionLayer>
+    {
+        [PropertyDescription]
+        public int Index { get; set; }
+        
+        [PropertyDescription]
+        public int MyAgeGroup { get; set; }
+        
+        [PropertyDescription]
+        public int MyStage { get; set; }
+        
+        [PropertyDescription] 
+        public UnregisterAgent UnregisterHandle { get; set; }
+
+        public void Init(InfectionLayer layer)
+        {
+            _infectionLayer = layer;
+            _infectionLayer.ArrayAgeGroups[Index] = MyAgeGroup; 
+        }
+
+        public void Tick()
+        {
+            Interact();
+            if (MyStage != (int)Stage.Recovered) Progress();
+            if (MyStage == (int)Stage.Mortality) Die();
+        }
+
+        private void Interact()
+        {
+            if (_infectionLayer.ContactEnvironment.Interact(Index)) MyStage = (int)Stage.Exposed;
+        }
+
+        private void Die()
+        {
+           // UnregisterHandle.Invoke(_infectionLayer, this);
+        }
+
+        private void Progress()
+        {
+            MyStage = _infectionLayer.ArrayStages[Index];
+            // if (MyStage == (int)Stage.Recovered) Console.WriteLine("I'm recovered!!!");
+        }
+
+        private InfectionLayer _infectionLayer;
+
+        public Guid ID { get; set; }
+    }
+    
+    
+}
---
+++ EpidemicSpread/Model/TensorGraphEnvironment.cs
@@ -0,0 +1,45 @@
+using System;
+using Tensorflow;
+using static Tensorflow.Binding;
+
+namespace EpidemicSpread.Model;
+
+public abstract class TensorGraphEnvironment
+{
+    private Tensor Aggregate(Tensor messages, Tensor targetIndices, int numNodes)
+    {
+        // [4180] Messages
+        // [1000] Nodes
+        tf.print(tf.shape(messages));
+        tf.print(tf.shape(targetIndices));
+        // [4180 1000]
+        var oneHotIndices = tf.one_hot(targetIndices, depth: numNodes);
+        tf.print(tf.shape(oneHotIndices));
+        var expandedMessages = tf.expand_dims(messages, axis: -1);
+        var weightedMessages = expandedMessages * oneHotIndices;
+        
+        var aggregatedMessages = tf.reduce_sum(weightedMessages, axis: 0);
+        tf.print(tf.shape(aggregatedMessages));
+        return aggregatedMessages;
+    }
+
+    protected Tensor Propagate(Tensor edgeIndex, Tensor nodeFeatures, params object[] args)
+    {
+        // edgeIndex: [2, 4180]
+        // collect all infected indices for each target Node in a tensor
+        var sourceNode = tf.gather(edgeIndex, tf.constant(0));
+        var targetNode = tf.gather(edgeIndex, tf.constant(1));
+        var sourceFeature = tf.gather(nodeFeatures, sourceNode);
+        var targetFeature = tf.gather(nodeFeatures, targetNode);
+        
+        // calculation of the transmission chance(message) for every edge
+        var edgeMessages = Message(sourceFeature, targetFeature, (int) args[0]);
+        // allocation of the messages to the target nodes
+        var outFeatures = Aggregate(edgeMessages, targetNode, (int)nodeFeatures.shape[0]);
+        return outFeatures;
+        
+        
+    }
+
+    protected abstract Tensor Message(Tensor sourceNode, Tensor targetNode, int currentTick);
+}
